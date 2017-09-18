659a9dbf9dc1c18829ed0930fdbeb9a54bd9f6ea
KrashMunoz <Krash.Munoz@gmail.com>
2017-09-18 18:52:55 -0400

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main( String[] args ){
+        System.out.println("Hello, World!");
+        System.out.println("Today is September 13, 2017. I am alive!");
+        System.out.println("My name is Krash.");
+    }
+}
